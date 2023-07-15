fa76512f07490da887612a118e3d7ba701e779b2
imrany <imranmat254@gmail.com>
2023-07-15 21:41:28 +0300

    lazy loading done

--- src/components/DogList.vue
+++ src/components/DogList.vue
@@ -4,11 +4,10 @@
              <h1 class="text-center mb-4 text-5xl font-semibold">Dog Images</h1>
             <input type="text" name="search" id="search" placeholder="Search for a dog / breed" class="py-2 border-gray-500 rounded-[8px] placeholder:text-black focus:outline-orange-900 border-[1px] px-4 w-[50vw]"/>
         </div>
-        <img src=".../assets/loadingDog.jpg" alt="doj">
         <div class="mt-10 grid grid-cols-3 gap-y-2 gap-x-4 mx-20">
             <div class="" v-for="(image,index) in dogImages" :key="index">
                 <router-link to="/dog/1">
-                    <img v-lazy="{ src: image, loading: '@/assets/loadingDog.jpg', error: '.@/assets/loadingDog.jpg' }" :src="image" class="h-[300px] w-[380px]" alt="Dog">
+                    <img v-lazy="{ src: image, loading: loadingImage, error: loadingImage }" :src="image" class="h-[300px] w-[380px]" alt="Dog">
                 </router-link>
             </div>
         </div>
@@ -18,11 +17,13 @@
 <script>
 import { mapGetters } from 'vuex';
 import DogService from '../services/DogService';
+import loadingImage from "../assets/loadingDog.jpg";
 
 export default {
     data(){
         return {
             dogImages:[],
+            loadingImage
         };
     },
     computed: {
